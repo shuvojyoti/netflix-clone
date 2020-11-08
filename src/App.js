e7cdd9ec6b6256497e10ac26924450909e266f64
Shuvo <shuvojyoti.golder@gmail.com>
2020-11-09 00:03:15 +0600

    Row completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
-import Row from './Row'
-import requests from './requests'
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from NETFLIX Clone</h1>
-      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,27 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+const baseurl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState ([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(()=> {
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
 
-        async function fetchData (){
+    fetchData();
+  }, []);
 
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            return request;
-
-        }
-
-        fetchData();
-
-    }, []);
-    return (
-        <div>
-            <h2>{title}</h2>
-        </div>
-    )
+  console.log(movies);
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="row-posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${baseurl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
