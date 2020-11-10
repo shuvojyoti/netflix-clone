71d81a99c537b087a31172bbb2a8d85c2e838527
shuvojyoti <shuvojyoti.golder@gmail.com>
2020-11-10 12:55:29 +0600

    Header fixed

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
